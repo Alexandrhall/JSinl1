6253ccf05d28b9910383fd7b1eb678eed6048fdb
Alexandrhall <91125828+Alexandrhall@users.noreply.github.com>
2021-11-04 20:14:33 +0100

    fixad funktioner

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 window.onload = function () {
     createHTML();
     myList();
-    let button = document.getElementById("button");
-    button.addEventListener("click", adinList);
+    document.getElementById("button").addEventListener("click", adinList);
+    document.getElementById("inp").addEventListener("blur", blurIt);
 };
 
 let list = [];
@@ -11,15 +11,34 @@ function createHTML() {
     let input = document.createElement("input");
     input.className = "input";
     input.placeholder = "Write new..";
+    input.type = "text";
+    input.id = "inp";
     let headwrap = document.getElementById("headwrap");
     headwrap.appendChild(input);
     let button = document.createElement("button");
     button.innerHTML = "Add";
     button.id = "button";
     headwrap.appendChild(button);
+    let saveBut = document.createElement("button");
+    saveBut.innerHTML = "Save";
+    headwrap.appendChild(saveBut);
 }
 
-function adinList() {}
+function adinList() {
+    let task = document.getElementById("inp").value;
+    let mylistUl = document.getElementById("myList");
+
+    let li = document.createElement("li");
+    let v = document.createTextNode(task);
+    new taskList(task);
+
+    if (task == "") {
+        alert("Write someting");
+    }
+
+    li.innerHTML = list[list.length - 1].task;
+    mylistUl.appendChild(li);
+}
 
 function myList() {
     let mylistUl = document.getElementById("myList");
@@ -33,6 +52,17 @@ function myList() {
         a.innerHTML = list[i].task;
         mylistUl.appendChild(a);
     }
+}
+
+function blurIt() {
+    let inp = document.getElementById("inp");
+    let button = document.getElementById("button");
+    button.onclick = function () {
+        inp.value = "";
+    };
+    setTimeout(function () {
+        inp.value = "";
+    }, 300);
 }
 
 class taskList {
